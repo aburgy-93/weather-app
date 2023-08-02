4d60db1206a792947d3265d09edd7e9b4516ae22
aburgy-93 <aburgy@uwalumni.com>
2023-08-01 19:37:23 -0500

    changes added

--- src/views/weatherView.js
+++ src/views/weatherView.js
@@ -10,18 +10,14 @@ class WeatherView extends View {
   }
 
   _generateMarkup() {
-    // return `
-    //   <img src="${this._data.condition.conditionIcon}" class="weather-icon">
-    //   <h1 class="location-heading">${this._data.location.name}, ${this._data.location.region}</h1>
-    //   <h2 class="weather-description">${this._data.current.currWeather}</h2>
-    //   <h2 class="temp">${this._data.current.currTempF}<span>&#8457;</span></h2>
-    // `;
     return `
     <img src="${this._data.condition.conditionIcon}" class="weather-icon"> 
     <div class="weaither-info">
       <ion-icon name="accessibility-outline"></ion-icon> 
       <div class="location">
-      <h1 class="location-heading">${this._data.location.name}, ${this._data.location.region}</h1>
+      <h1 class="location-heading">${this._data.location.name}, ${
+      this._data.location.region
+    }</h1>
       </div>
       <h2 class="weather-description">${this._data.current.currWeather}</h2>
       <h2 class="temp">${this._data.current.currTempF}<span>&#8457;</span></h2>
@@ -54,17 +50,7 @@ class WeatherView extends View {
         <p class="hourly-title">Hourly Forecast</p>
       </div>
       <ul class="hourly">
-        <li class="preview">
-          <p class="hour">13</p>
-          <img src="img/images/humidity.png" alt="" class="hourImg">
-          <p class="hourlyTemp">74<span>&#8457;</span></p>
-        </li>
-        
-        <li class="preview">
-          <p class="hour">24</p>
-          <img  src="img/images/humidity.png" alt="" class="hourImg">
-          <p class="hourlyTemp">74<span>&#8457;</span></p>
-        </li> 
+         ${this._data.hourlyForcast.map(this._generateHourlyMarkup).join("")}
       </ul>
     </div>
     
@@ -74,27 +60,31 @@ class WeatherView extends View {
         <p class="forecast-title">3-day Forecast</p>
       </div>
       <div class="forecast-container">
-        <div class="day day-1">
-          <p>Mon</p>
-          <img src="img/images/clouds.png" alt="">
-          <p>50<span>&#8457;</span></p>
-          <p>50<span>&#8457;</span></p>
-        </div>
-        <div class="day day-2">
-          <p>Tues</p>
-          <img src="img/images/clouds.png" alt="">
-          <p>60<span>&#8457;</span></p>
-          <p>60<span>&#8457;</span></p>
-        </div>
-        <div class="day day-3">
-          <p>Wed</p>
-          <img src="img/images/clouds.png" alt="">
-          <p>69<span>&#8457;</span></p>
-          <p>69<span>&#8457;</span></p>
-        </div> 
+        ${this._data.forecastResults.map(this._generateForecastMarkup).join("")}
       </div>
     </div>
   `;
+  }
+
+  _generateHourlyMarkup(data) {
+    return `
+        <li class="preview">
+          <p class="hour">${data.time}</p>
+          <img src="${data.condition}" alt="" class="hourImg">
+          <p class="hourlyTemp">${data.temp}<span>&#8457;</span></p>
+        </li>
+      `;
+  }
+
+  _generateForecastMarkup(day) {
+    return `
+            <div class="day day-">
+              <p>${day.date}</p>
+              <img src="${day.dayIcon}" alt="">
+              <p>${Math.trunc(day.hightemp_f)}<span>&#8457;</span></p>
+              <p>${Math.trunc(day.lowtemp_f)}<span>&#8457;</span></p>
+            </div>
+          `;
   }
 }
 
